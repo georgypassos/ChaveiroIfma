327b9f84e0adb878f213276e76ded2314af98ea5
Georgy Passos <georgy1101@gmail.com>
2012-06-19 12:20:40 -0300

    commt

--- src/Classe1.java
+++ src/Classe1.java
@@ -1,6 +1,6 @@
 
 public class Classe1 {
 	public static void main(String[] args) {
-		//comentario fisjdahofiuaenhocifuahoufcousncafo ashouisda o8ufybadsuyb f
+		//comentario fisjdahofiuaenhocifuahoufcousncafo 
 	}
 }
